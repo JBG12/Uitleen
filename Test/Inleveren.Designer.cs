c136cb80375fc695e1e0beaa993dcc5ac1dd6d35
Wouter <74597342+wouterroolvink@users.noreply.github.com>
2021-03-08 10:20:59 +0100

    nieuw design inleveren

--- Test/Inleveren.Designer.cs
+++ Test/Inleveren.Designer.cs
@@ -39,13 +39,16 @@ namespace uitleen
             this.button2 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.opmerkingt = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(434, 222);
+            this.label2.Location = new System.Drawing.Point(158, 111);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(50, 13);
             this.label2.TabIndex = 1;
@@ -55,7 +58,7 @@ namespace uitleen
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(434, 275);
+            this.label3.Location = new System.Drawing.Point(158, 164);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(81, 13);
             this.label3.TabIndex = 2;
@@ -65,7 +68,7 @@ namespace uitleen
             // apparaatt
             // 
             this.apparaatt.AccessibleName = "apparaat";
-            this.apparaatt.Location = new System.Drawing.Point(555, 222);
+            this.apparaatt.Location = new System.Drawing.Point(279, 111);
             this.apparaatt.Name = "apparaatt";
             this.apparaatt.Size = new System.Drawing.Size(100, 20);
             this.apparaatt.TabIndex = 4;
@@ -74,7 +77,7 @@ namespace uitleen
             // leerlingnummert
             // 
             this.leerlingnummert.AccessibleName = "leerlingnummer";
-            this.leerlingnummert.Location = new System.Drawing.Point(555, 275);
+            this.leerlingnummert.Location = new System.Drawing.Point(279, 164);
             this.leerlingnummert.Name = "leerlingnummert";
             this.leerlingnummert.Size = new System.Drawing.Size(100, 20);
             this.leerlingnummert.TabIndex = 5;
@@ -82,15 +85,15 @@ namespace uitleen
             // panel1
             // 
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Location = new System.Drawing.Point(3, 59);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(393, 425);
+            this.panel1.Size = new System.Drawing.Size(116, 386);
             this.panel1.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(39, 39);
+            this.label4.Location = new System.Drawing.Point(8, 22);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(102, 13);
             this.label4.TabIndex = 0;
@@ -98,7 +101,7 @@ namespace uitleen
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(606, 332);
+            this.button1.Location = new System.Drawing.Point(308, 339);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 88);
             this.button1.TabIndex = 7;
@@ -108,7 +111,7 @@ namespace uitleen
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(468, 332);
+            this.button2.Location = new System.Drawing.Point(170, 339);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(132, 88);
             this.button2.TabIndex = 8;
@@ -120,7 +123,7 @@ namespace uitleen
             // 
             this.label5.AccessibleName = "opmerking";
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(692, 190);
+            this.label5.Location = new System.Drawing.Point(293, 202);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(70, 13);
             this.label5.TabIndex = 9;
@@ -130,18 +133,42 @@ namespace uitleen
             // opmerkingt
             // 
             this.opmerkingt.AccessibleName = "opmerking";
-            this.opmerkingt.Location = new System.Drawing.Point(677, 222);
+            this.opmerkingt.Location = new System.Drawing.Point(278, 234);
             this.opmerkingt.Multiline = true;
             this.opmerkingt.Name = "opmerkingt";
             this.opmerkingt.Size = new System.Drawing.Size(101, 73);
             this.opmerkingt.TabIndex = 10;
             // 
-            // Form1
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(-3, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(768, 56);
+            this.panel2.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(55, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(387, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Uitleensregistratiesysteem";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // inleveren
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Cyan;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(445, 450);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.opmerkingt);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button2);
@@ -151,11 +178,13 @@ namespace uitleen
             this.Controls.Add(this.apparaatt);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Name = "Form1";
+            this.Name = "inleveren";
             this.Text = "inleveren";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,6 +201,8 @@ namespace uitleen
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox opmerkingt;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
