506131b3063c8804b4654c981ccd8a25db7a098c
JBG12 <234269@edu.rocfriesepoort.nl>
2021-03-08 11:35:40 +0100

    opmerkingen -> opmerking

--- Test/Apparatenlijst.cs
+++ Test/Apparatenlijst.cs
@@ -125,7 +125,7 @@ namespace Test
                     resultlist[0].Add(dataReader["apparaatnaam"] + "");
                     resultlist[1].Add(dataReader["uitgeleend"] + "");
                     resultlist[2].Add(dataReader["uitlener"] + "");
-                    resultlist[3].Add(dataReader["opmerkingen"] + "");
+                    resultlist[3].Add(dataReader["opmerking"] + "");
                 }
 
                 // We zijn klaar met het lezen dus we kunnen hier de datareader sluiten.
